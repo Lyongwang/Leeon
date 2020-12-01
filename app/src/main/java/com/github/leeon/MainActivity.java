b052ff42f271c40fd08cc91ef899405e05c2d15e
liyongwang <liyongwang@yiche.com>
2020-12-01 14:36:59 +0800

    add : 热修复替换dex

--- app/src/main/java/com/github/leeon/MainActivity.java
+++ app/src/main/java/com/github/leeon/MainActivity.java
@@ -11,8 +11,18 @@ import com.github.common.router.RouterConstans;
 import com.github.leeon.widget.SrcScrollFrameLayout;
 import com.github.loginlib.view.activity.LoginActivity;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import dalvik.system.BaseDexClassLoader;
+import dalvik.system.PathClassLoader;
+import okio.BufferedSink;
+import okio.Okio;
+import okio.Sink;
+import okio.Source;
 
 /**
  * @author lxiansheng
@@ -56,6 +66,51 @@ public class MainActivity extends AppCompatActivity {
 //                return 0;
 //            }
 //        });
+
+        findViewById(R.id.btn_add_app).setOnClickListener(view -> {
+            tvFirstPage.setText("fix");
+        });
+
+        findViewById(R.id.btn_add_fix).setOnClickListener(view -> {
+            loadClassesFromFix();
+        });
+    }
+
+    private void loadClassesFromFix() {
+        String fileName = "fix.png";
+        try(Source source = Okio.source(getAssets().open(fileName));
+            Sink sink = Okio.sink(new File(getFilesDir(), fileName));
+            BufferedSink buffer = Okio.buffer(sink)){
+            buffer.writeAll(source);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BaseDexClassLoader classLoader = (BaseDexClassLoader) getClassLoader();
+        PathClassLoader fixClassLoader = new PathClassLoader(getFilesDir().getPath() + File.separator + fileName, null);
+        Class loaderClass = BaseDexClassLoader.class;
+
+        try {
+            Field pathListField = loaderClass.getDeclaredField("pathList");
+            pathListField.setAccessible(true);
+            // getClassLoader().pathList
+            Object loaderPathList = pathListField.get(classLoader);
+            // fixClassLoader.pathList
+            final Object fixPathList = pathListField.get(fixClassLoader);
+
+            Class pathListClass = loaderPathList.getClass();
+
+            Field dexElementsField = pathListClass.getField("dexElements");
+            dexElementsField.setAccessible(true);
+            // fixClassLoader.pathList.dexElements
+            Object fixDexElements = dexElementsField.get(fixPathList);
+            // classLoader.pathList.dexElements = fixClassLoader.pathList.dexElements;
+            dexElementsField.set(loaderPathList, fixDexElements);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
     private void callMyMethod() {
